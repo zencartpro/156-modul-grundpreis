61d038022b1d5c84502d6d6bd12c80b50b2c8923
webchills <office@webchills.com>
2019-07-21 11:35:56 +0200

    Version 2.7.0

---
+++ NEUE DATEIEN/includes/index_filters/extra_products_filter.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package Grundpreis 
+ * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
+ * @version $Id: extra_products_filters.php 2018-06-26 16:19:14 webchills $
+*/
+// show more details of a specified product
+$extra_products_details .= ' p.products_base_unit_price, p.products_base_unit,';
